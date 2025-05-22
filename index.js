fd561d473a3acf3fb958c22cba7b08a7198e8f5a
Eddie <eddie123burciaga@gmail.com>
2025-05-22 13:24:53 -0600

    velocidad (nombre completo)

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(express.json());
 app.get("/temperatura", (req, res) => {
   res.json({ valor: "10 °C", timestamp: new Date().toISOString() });
 });
+app.get("/velocidad", (req, res) => {
+  res.json({ nombre: "Eddie", apellido: "Burciaga" });
+});
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
